237a14519d80e0c986da26164b470cfcdddd76ee
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2023-07-06 12:36:12 +0200

    Update func.py

--- src/func.py
+++ src/func.py
@@ -1,8 +1,9 @@
 from symai import Expression, Function
 
 
-MAIL_CONTEXT = """Improve the writing style, grammar and issue in the following email.
-Make the email sound more {0}:
+MAIL_CONTEXT = """Improve the writing style, grammar and spelling issue in the following email.
+Also properly format the structure of the email with subject, signature, etc.
+Make the email have more `{0}` reading language:
 """
 
 
