99f4089c58bdfa2c0dc1424f167794453349edb3
Xpitfire <dinu.marius-constantin@hotmail.com>
2023-07-11 12:44:32 +0200

    added format support

--- src/func.py
+++ src/func.py
@@ -3,7 +3,7 @@ from symai import Expression, Function
 
 MAIL_CONTEXT = """Improve the writing style, grammar and spelling issue in the following email.
 Also properly format the structure of the email with subject, signature, etc.
-Make the email have more `{0}` reading language:
+Make the email have more `{style}` reading language:
 """
 
 
@@ -14,5 +14,5 @@ class EmailWriter(Expression):
 
     def forward(self, x, style='formal', *args, **kwargs):
         x = self._to_symbol(x)
-        self.fn.prompt.format(style)
+        self.fn.format(style=style)
         return self.fn(x, *args, **kwargs)
